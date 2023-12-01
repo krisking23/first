1eaced5c63c5e585e3509e5c5ca6db1bdbe7c07e
Baron <bkkcreative23@gmail.com>
2023-12-01 09:24:58 -0600

    added code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hi")
+def count_words(str):
+    return str.read().split()
+
+def count_letters(str):
+    dict = {}
+    
+    for word in str:
+        for letter in word:
+            letter = letter.lower()
+            val = dict.get(letter)
+            if val != None:
+                dict[letter] += 1
+            else:
+                dict[letter] = 1
+    return dict
+
+with open("./books/frankenstein.txt") as my_file:
+    contents = count_words(my_file)
+    letter_count = count_letters(contents)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(contents)} words found in the document\n\n")
+    for item in letter_count:
+        print(f"The {item} character was found {letter_count[item]} times")
+  
+
+
