89860ca54d5d32c09e8affa68682703750f28ce5
Baron <bkkcreative23@gmail.com>
2023-12-01 09:32:10 -0600

    only letters

--- main.py
+++ main.py
@@ -19,8 +19,19 @@ with open("./books/frankenstein.txt") as my_file:
     letter_count = count_letters(contents)
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{len(contents)} words found in the document\n\n")
+    
+    characters = []
+
     for item in letter_count:
-        print(f"The {item} character was found {letter_count[item]} times")
+        if item.isalpha():
+            characters.append(item)
+        
+    characters = sorted(characters)
+
+    for item in characters:
+        if item in letter_count:
+            print(f"The {item} character was found {letter_count[item]} times")
+
   
 
 
